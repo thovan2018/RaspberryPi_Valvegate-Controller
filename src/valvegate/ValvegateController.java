7912a6e8607fbe20d1902536d8bc8d34e9f7814a
Nguyen Van Tho <39107667+thovan2018@users.noreply.github.com>
2018-05-10 10:53:29 +0800

    Update ValvegateController.java

--- src/valvegate/ValvegateController.java
+++ src/valvegate/ValvegateController.java
@@ -76,6 +76,38 @@ public class ValvegateController implements Initializable {
     private ToggleButton tgglegate7;
     @FXML
     private ToggleButton tgglegate8;
+    @FXML
+    private ToggleButton tgglegate9;
+    @FXML
+    private ToggleButton tgglegate10;
+    @FXML
+    private ToggleButton tgglegate11;
+    @FXML
+    private ToggleButton tgglegate12;
+    @FXML
+    private ToggleButton tgglegate13;
+    @FXML
+    private ToggleButton tgglegate14;
+    @FXML
+    private ToggleButton tgglegate15;
+    @FXML
+    private ToggleButton tgglegate16;
+    @FXML
+    private ToggleButton tgglegate17;
+    @FXML
+    private ToggleButton tgglegate18;
+    @FXML
+    private ToggleButton tgglegate19;
+    @FXML
+    private ToggleButton tgglegate20;
+    @FXML
+    private ToggleButton tgglegate21;
+    @FXML
+    private ToggleButton tgglegate22;
+    @FXML
+    private ToggleButton tgglegate23;
+    @FXML
+    private ToggleButton tgglegate24;
     
     @FXML
     private TextField txtgate1T1;
@@ -141,6 +173,134 @@ public class ValvegateController implements Initializable {
     private TextField txtgate8T3;
     @FXML
     private TextField txtgate8T4;
+    @FXML
+    private TextField txtgate9T1;
+    @FXML
+    private TextField txtgate9T2;
+    @FXML
+    private TextField txtgate9T3;
+    @FXML
+    private TextField txtgate9T4;
+    @FXML
+    private TextField txtgate10T1;
+    @FXML
+    private TextField txtgate10T2;
+    @FXML
+    private TextField txtgate10T3;
+    @FXML
+    private TextField txtgate10T4;
+    @FXML
+    private TextField txtgate11T1;
+    @FXML
+    private TextField txtgate11T2;
+    @FXML
+    private TextField txtgate11T3;
+    @FXML
+    private TextField txtgate11T4;
+    @FXML
+    private TextField txtgate12T1;
+    @FXML
+    private TextField txtgate12T2;
+    @FXML
+    private TextField txtgate12T3;
+    @FXML
+    private TextField txtgate12T4;
+    @FXML
+    private TextField txtgate13T1;
+    @FXML
+    private TextField txtgate13T2;
+    @FXML
+    private TextField txtgate13T3;
+    @FXML
+    private TextField txtgate13T4;
+    @FXML
+    private TextField txtgate14T1;
+    @FXML
+    private TextField txtgate14T2;
+    @FXML
+    private TextField txtgate14T3;
+    @FXML
+    private TextField txtgate14T4;
+    @FXML
+    private TextField txtgate15T1;
+    @FXML
+    private TextField txtgate15T2;
+    @FXML
+    private TextField txtgate15T3;
+    @FXML
+    private TextField txtgate15T4;
+    @FXML
+    private TextField txtgate16T1;
+    @FXML
+    private TextField txtgate16T2;
+    @FXML
+    private TextField txtgate16T3;
+    @FXML
+    private TextField txtgate16T4;
+    @FXML
+    private TextField txtgate17T1;
+    @FXML
+    private TextField txtgate17T2;
+    @FXML
+    private TextField txtgate17T3;
+    @FXML
+    private TextField txtgate17T4;
+    @FXML
+    private TextField txtgate18T1;
+    @FXML
+    private TextField txtgate18T2;
+    @FXML
+    private TextField txtgate18T3;
+    @FXML
+    private TextField txtgate18T4;
+    @FXML
+    private TextField txtgate19T1;
+    @FXML
+    private TextField txtgate19T2;
+    @FXML
+    private TextField txtgate19T3;
+    @FXML
+    private TextField txtgate19T4;
+    @FXML
+    private TextField txtgate20T1;
+    @FXML
+    private TextField txtgate20T2;
+    @FXML
+    private TextField txtgate20T3;
+    @FXML
+    private TextField txtgate20T4;
+    @FXML
+    private TextField txtgate21T1;
+    @FXML
+    private TextField txtgate21T2;
+    @FXML
+    private TextField txtgate21T3;
+    @FXML
+    private TextField txtgate21T4;
+    @FXML
+    private TextField txtgate22T1;
+    @FXML
+    private TextField txtgate22T2;
+    @FXML
+    private TextField txtgate22T3;
+    @FXML
+    private TextField txtgate22T4;
+    @FXML
+    private TextField txtgate23T1;
+    @FXML
+    private TextField txtgate23T2;
+    @FXML
+    private TextField txtgate23T3;
+    @FXML
+    private TextField txtgate23T4;
+    @FXML
+    private TextField txtgate24T1;
+    @FXML
+    private TextField txtgate24T2;
+    @FXML
+    private TextField txtgate24T3;
+    @FXML
+    private TextField txtgate24T4;
     
     private TextField[][] textfield;
     
@@ -184,6 +344,10 @@ public class ValvegateController implements Initializable {
     private GridPane btnGridpane;
     @FXML
     private GridPane ftGridpane;
+    @FXML
+    private GridPane ftGridpane2;
+    @FXML
+    private GridPane ftGridpane3;
     
     @FXML
     private ToggleButton toggleGPIO0;
@@ -244,7 +408,7 @@ public class ValvegateController implements Initializable {
     private int year, month, day;
     private int hour, minute, second;
     
-    private int NumberOfGate=8;
+    private int NumberOfGate=16;
     private int NumberOfClosing=0;
         
     private Led indicatorGPIO0;
@@ -264,6 +428,22 @@ public class ValvegateController implements Initializable {
     private Led ftindicatorGPIO5;
     private Led ftindicatorGPIO6;
     private Led ftindicatorGPIO7;
+    private Led ftindicatorGPIO8;
+    private Led ftindicatorGPIO9;
+    private Led ftindicatorGPIO10;
+    private Led ftindicatorGPIO11;
+    private Led ftindicatorGPIO12;
+    private Led ftindicatorGPIO13;
+    private Led ftindicatorGPIO14;
+    private Led ftindicatorGPIO15;
+    private Led ftindicatorGPIO16;
+    private Led ftindicatorGPIO17;
+    private Led ftindicatorGPIO18;
+    private Led ftindicatorGPIO19;
+    private Led ftindicatorGPIO20;
+    private Led ftindicatorGPIO21;
+    private Led ftindicatorGPIO22;
+    private Led ftindicatorGPIO23;
     
     @Override 
     public void initialize(URL url, ResourceBundle rb){
@@ -276,42 +456,31 @@ public class ValvegateController implements Initializable {
             {txtgate5T1, txtgate5T2, txtgate5T3, txtgate5T4},
             {txtgate6T1, txtgate6T2, txtgate6T3, txtgate6T4},
             {txtgate7T1, txtgate7T2, txtgate7T3, txtgate7T4},
-            {txtgate8T1, txtgate8T2, txtgate8T3, txtgate8T4}
+            {txtgate8T1, txtgate8T2, txtgate8T3, txtgate8T4},
+            {txtgate9T1, txtgate9T2, txtgate9T3, txtgate9T4},
+            {txtgate10T1, txtgate10T2, txtgate10T3, txtgate10T4},
+            {txtgate11T1, txtgate11T2, txtgate11T3, txtgate11T4},
+            {txtgate12T1, txtgate12T2, txtgate12T3, txtgate12T4},
+            {txtgate13T1, txtgate13T2, txtgate13T3, txtgate13T4},
+            {txtgate14T1, txtgate14T2, txtgate14T3, txtgate14T4},
+            {txtgate15T1, txtgate15T2, txtgate15T3, txtgate15T4},
+            {txtgate16T1, txtgate16T2, txtgate16T3, txtgate16T4},
+            {txtgate17T1, txtgate17T2, txtgate17T3, txtgate17T4},
+            {txtgate18T1, txtgate18T2, txtgate18T3, txtgate18T4},
+            {txtgate19T1, txtgate19T2, txtgate19T3, txtgate19T4},
+            {txtgate20T1, txtgate20T2, txtgate20T3, txtgate20T4},
+            {txtgate21T1, txtgate21T2, txtgate21T3, txtgate21T4},
+            {txtgate22T1, txtgate22T2, txtgate22T3, txtgate22T4},
+            {txtgate23T1, txtgate23T2, txtgate23T3, txtgate23T4},
+            {txtgate24T1, txtgate24T2, txtgate24T3, txtgate24T4}
         };
         // using virtual numeric keypad for textfields
-        txtgate1T1.getProperties().put("vkType", "numeric");
-        txtgate1T2.getProperties().put("vkType", "numeric");
-        txtgate1T3.getProperties().put("vkType", "numeric");
-        txtgate1T4.getProperties().put("vkType", "numeric");
-        txtgate2T1.getProperties().put("vkType", "numeric");
-        txtgate2T2.getProperties().put("vkType", "numeric");
-        txtgate2T3.getProperties().put("vkType", "numeric");
-        txtgate2T4.getProperties().put("vkType", "numeric");
-        txtgate3T1.getProperties().put("vkType", "numeric");
-        txtgate3T2.getProperties().put("vkType", "numeric");
-        txtgate3T3.getProperties().put("vkType", "numeric");
-        txtgate3T4.getProperties().put("vkType", "numeric");
-        txtgate4T1.getProperties().put("vkType", "numeric");
-        txtgate4T2.getProperties().put("vkType", "numeric");
-        txtgate4T3.getProperties().put("vkType", "numeric");
-        txtgate4T4.getProperties().put("vkType", "numeric");
-        txtgate5T1.getProperties().put("vkType", "numeric");
-        txtgate5T2.getProperties().put("vkType", "numeric");
-        txtgate5T3.getProperties().put("vkType", "numeric");
-        txtgate5T4.getProperties().put("vkType", "numeric");
-        txtgate6T1.getProperties().put("vkType", "numeric");
-        txtgate6T2.getProperties().put("vkType", "numeric");
-        txtgate6T3.getProperties().put("vkType", "numeric");
-        txtgate6T4.getProperties().put("vkType", "numeric");
-        txtgate7T1.getProperties().put("vkType", "numeric");
-        txtgate7T2.getProperties().put("vkType", "numeric");
-        txtgate7T3.getProperties().put("vkType", "numeric");
-        txtgate7T4.getProperties().put("vkType", "numeric");
-        txtgate8T1.getProperties().put("vkType", "numeric");
-        txtgate8T2.getProperties().put("vkType", "numeric");
-        txtgate8T3.getProperties().put("vkType", "numeric");
-        txtgate8T4.getProperties().put("vkType", "numeric");
-        
+        for(int i=0; i<24; i++){
+            for(int j=0; j<4; j++){
+                textfield[i][j].getProperties().put("vkType", "numeric");
+            }
+        }
+                
         // Set background color for buttons Power Off and Restart
         btnPowerOff.setStyle("-fx-font: bold 13pt \"Arial\";"
                 + "-fx-background-color: #ff0000; "
@@ -344,14 +513,12 @@ public class ValvegateController implements Initializable {
         
                 
         // initializing array "togglebtn"
-        togglebtn = new ToggleButton[]{tgglegate1,
-            tgglegate2,
-            tgglegate3,
-            tgglegate4,
-            tgglegate5,
-            tgglegate6,
-            tgglegate7,
-            tgglegate8
+        togglebtn = new ToggleButton[]{tgglegate1,tgglegate2,tgglegate3,tgglegate4,
+            tgglegate5,tgglegate6,tgglegate7,tgglegate8,
+            tgglegate9,tgglegate10,tgglegate11,tgglegate12,
+            tgglegate13,tgglegate14,tgglegate15,tgglegate16,
+            tgglegate17,tgglegate18,tgglegate19,tgglegate20,
+            tgglegate21,tgglegate22,tgglegate23,tgglegate24
         };
         
         // initializing array "group"
@@ -399,13 +566,23 @@ public class ValvegateController implements Initializable {
         
         
         // initializing toggle button for each gate
-        for(int i=0; i<togglebtn.length; i++){
-            togglebtn[i].setSelected(true);
+        // Set G1 - G16 are TRUE and G17 - G24 are FALSE at initializing
+        for(int i=0; i<24; i++){
+            if(i<16){
+                togglebtn[i].setSelected(true);
+            }else{
+                togglebtn[i].setSelected(false);
+                for(int j=0; j<4; j++){
+                    textfield[i][j].opacityProperty().setValue(0.3);
+                    textfield[i][j].setText("0");
+                }
+            }
         }
-        
-        for(int i=0; i<8; i++){
+               
+        // Set property for toggle button tgglegate1
+        for(int i=0; i<24; i++){
             final int k=i;
-            togglebtn[i].setOnAction((ActionEvent event)->{
+            togglebtn[k].setOnAction((ActionEvent event)->{
                 if(togglebtn[k].isSelected()){
                     if(rbT12.isSelected()){
                         for(int j=0; j<2; j++){
@@ -428,38 +605,7 @@ public class ValvegateController implements Initializable {
                 }
             });
         }
-        /*
-        // Set property for toggle button tgglegate1              
-        for(int i=0; i<8; i++){
-            final int k=i;
-            group[k].selectedToggleProperty().addListener((ObservableValue<? extends Toggle> ov, Toggle oldValue, Toggle newValue) -> {
-                if (newValue == null){//IF the toggle 1 is deactivated
-                    for (int j=0; j<4; j++){
-                        textfield[k][j].opacityProperty().setValue(0.3); // the corresponding textfields are blurred
-                        textfield[k][j].setText("0");                    // set the values of textfields are 0
-                    }
-                    NumberOfGate -=1; // decrease the number of activated gates
-                    lblActiveGate.setText(String.valueOf(NumberOfGate)); // update the value on the screen
-                }
-                else{// IF the toggle 1 is activated again
-                    if ("T1-T2".equals(menubtnSequence.getText())){
-                        for(int j=0; j<2; j++){
-                            textfield[k][j].opacityProperty().setValue(1);
-                        }
-                    }
-                    else{
-                        for(int j=0; j<4; j++){
-                            textfield[k][j].opacityProperty().setValue(1);
-                        }                        
-                    }
-                    NumberOfGate +=1; // increase the number of activated gates
-                    lblActiveGate.setText(String.valueOf(NumberOfGate));// update the value on the screen
-                }
-            });
-        }*/
-        // Set Default item for ListView
-        //dataList.
-        SessionList.setItems(dataList);
+        
         
         // Display time
         Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {            
@@ -507,6 +653,22 @@ public class ValvegateController implements Initializable {
         ftindicatorGPIO5 = createIndicator();
         ftindicatorGPIO6 = createIndicator();
         ftindicatorGPIO7 = createIndicator();
+        ftindicatorGPIO8 = createIndicator();
+        ftindicatorGPIO9 = createIndicator();
+        ftindicatorGPIO10 = createIndicator();
+        ftindicatorGPIO11 = createIndicator();
+        ftindicatorGPIO12 = createIndicator();
+        ftindicatorGPIO13 = createIndicator();
+        ftindicatorGPIO14 = createIndicator();
+        ftindicatorGPIO15 = createIndicator();
+        ftindicatorGPIO16 = createIndicator();
+        ftindicatorGPIO17 = createIndicator();
+        ftindicatorGPIO18 = createIndicator();
+        ftindicatorGPIO19 = createIndicator();
+        ftindicatorGPIO20 = createIndicator();
+        ftindicatorGPIO21 = createIndicator();
+        ftindicatorGPIO22 = createIndicator();
+        ftindicatorGPIO23 = createIndicator();
         
         btnGridpane.add(indicatorGPIO0, 0, 0);
         btnGridpane.add(indicatorGPIO1, 1, 0);
@@ -517,6 +679,7 @@ public class ValvegateController implements Initializable {
         btnGridpane.add(indicatorGPIO6, 6, 0);
         btnGridpane.add(indicatorGPIO7, 7, 0);
         
+        // adding LEDs to Set 1
         ftGridpane.add(ftindicatorGPIO0, 2, 1);
         ftGridpane.add(ftindicatorGPIO1, 4, 1);
         ftGridpane.add(ftindicatorGPIO2, 6, 1);
@@ -525,6 +688,27 @@ public class ValvegateController implements Initializable {
         ftGridpane.add(ftindicatorGPIO5, 4, 3);
         ftGridpane.add(ftindicatorGPIO6, 6, 3);
         ftGridpane.add(ftindicatorGPIO7, 8, 3);
+        
+        // adding LEDs to Set 2
+        ftGridpane2.add(ftindicatorGPIO8, 2, 1);
+        ftGridpane2.add(ftindicatorGPIO9, 4, 1);
+        ftGridpane2.add(ftindicatorGPIO10, 6, 1);
+        ftGridpane2.add(ftindicatorGPIO11, 8, 1);
+        ftGridpane2.add(ftindicatorGPIO12, 2, 3);
+        ftGridpane2.add(ftindicatorGPIO13, 4, 3);
+        ftGridpane2.add(ftindicatorGPIO14, 6, 3);
+        ftGridpane2.add(ftindicatorGPIO15, 8, 3);
+        
+        // adding LEDs to Set 3
+        ftGridpane3.add(ftindicatorGPIO16, 2, 1);
+        ftGridpane3.add(ftindicatorGPIO17, 4, 1);
+        ftGridpane3.add(ftindicatorGPIO18, 6, 1);
+        ftGridpane3.add(ftindicatorGPIO19, 8, 1);
+        ftGridpane3.add(ftindicatorGPIO20, 2, 3);
+        ftGridpane3.add(ftindicatorGPIO21, 4, 3);
+        ftGridpane3.add(ftindicatorGPIO22, 6, 3);
+        ftGridpane3.add(ftindicatorGPIO23, 8, 3);
+        
         //indicatorGPIO0.onProperty().bindBidirectional(null);
                 
         indicatorGPIO0.onProperty().
@@ -567,6 +751,40 @@ public class ValvegateController implements Initializable {
         ftindicatorGPIO7.onProperty().
                 bindBidirectional(gpioadapterFX.gpio7StateProperty());
         
+        ftindicatorGPIO8.onProperty().
+                bindBidirectional(gpioadapterFX.gpio8StateProperty());
+        ftindicatorGPIO9.onProperty().
+                bindBidirectional(gpioadapterFX.gpio9StateProperty());
+        ftindicatorGPIO10.onProperty().
+                bindBidirectional(gpioadapterFX.gpio10StateProperty());
+        ftindicatorGPIO11.onProperty().
+                bindBidirectional(gpioadapterFX.gpio11StateProperty());
+        ftindicatorGPIO12.onProperty().
+                bindBidirectional(gpioadapterFX.gpio12StateProperty());
+        ftindicatorGPIO13.onProperty().
+                bindBidirectional(gpioadapterFX.gpio13StateProperty());
+        ftindicatorGPIO14.onProperty().
+                bindBidirectional(gpioadapterFX.gpio14StateProperty());
+        ftindicatorGPIO15.onProperty().
+                bindBidirectional(gpioadapterFX.gpio15StateProperty());
+        
+        ftindicatorGPIO16.onProperty().
+                bindBidirectional(gpioadapterFX.gpio16StateProperty());
+        ftindicatorGPIO17.onProperty().
+                bindBidirectional(gpioadapterFX.gpio17StateProperty());
+        ftindicatorGPIO18.onProperty().
+                bindBidirectional(gpioadapterFX.gpio18StateProperty());
+        ftindicatorGPIO19.onProperty().
+                bindBidirectional(gpioadapterFX.gpio19StateProperty());
+        ftindicatorGPIO20.onProperty().
+                bindBidirectional(gpioadapterFX.gpio20StateProperty());
+        ftindicatorGPIO21.onProperty().
+                bindBidirectional(gpioadapterFX.gpio21StateProperty());
+        ftindicatorGPIO22.onProperty().
+                bindBidirectional(gpioadapterFX.gpio22StateProperty());
+        ftindicatorGPIO23.onProperty().
+                bindBidirectional(gpioadapterFX.gpio23StateProperty());
+        
         toggleGPIO0.selectedProperty().
                 bindBidirectional(gpioadapterFX.gpio0StateProperty());
         toggleGPIO1.selectedProperty().
@@ -594,7 +812,7 @@ public class ValvegateController implements Initializable {
     }
     
     private void menuItemActive(){
-        for(int i=0; i<8; i++){
+        for(int i=0; i<24; i++){
             for(int j=2; j<4; j++){
                 textfield[i][j].editableProperty().setValue(Boolean.TRUE);
                 textfield[i][j].opacityProperty().setValue(1.0);
@@ -603,7 +821,7 @@ public class ValvegateController implements Initializable {
     }
     
     private void menuItemdeActive(){    
-        for(int i=0; i<8; i++){
+        for(int i=0; i<24; i++){
             for(int j=2; j<4; j++){
                 textfield[i][j].editableProperty().setValue(Boolean.FALSE);
                 textfield[i][j].opacityProperty().setValue(0.3);
